efd887a35ac9fcac234d4acefa1fa165317c7e94
boun <143073009+bounpherng@users.noreply.github.com>
2025-08-26 09:14:00 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'online-system-cache-v3.6.1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  'https://i.ibb.co/F4VGPVB1/image.png',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@400;500;700&display=swap'
+];
