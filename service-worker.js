09b73fb42d9e6bad87c1226b0da62e7477d028c0
boun <143073009+bounpherng@users.noreply.github.com>
2025-08-27 11:44:54 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'online-system-cache-v3.6.1';
+const CACHE_NAME = 'online-system-cache-v3.6.2';
 const urlsToCache = [
   './',
   './index.html',
@@ -6,4 +6,5 @@ const urlsToCache = [
   './icon-512.png',
   'https://i.ibb.co/F4VGPVB1/image.png',
   'https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@400;500;700&display=swap'
+
 ];
